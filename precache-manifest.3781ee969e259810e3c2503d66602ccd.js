301336e8d03c86d86be7ede8a6c8658b27e5a4c4
kunalkr789 <55917079+kunalkr789@users.noreply.github.com>
2020-08-10 18:36:12 +0530

    Updates

---
+++ precache-manifest.3781ee969e259810e3c2503d66602ccd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "593140ec58a9cd33b0e3b921694c4b35",
+    "url": "/MW_assignment/index.html"
+  },
+  {
+    "revision": "0f574ff5c31b580ee301",
+    "url": "/MW_assignment/static/css/2.93fbca5d.chunk.css"
+  },
+  {
+    "revision": "e609095d2f6433ff9665",
+    "url": "/MW_assignment/static/css/main.6fdf588d.chunk.css"
+  },
+  {
+    "revision": "0f574ff5c31b580ee301",
+    "url": "/MW_assignment/static/js/2.ea75857d.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/MW_assignment/static/js/2.ea75857d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e609095d2f6433ff9665",
+    "url": "/MW_assignment/static/js/main.173f7506.chunk.js"
+  },
+  {
+    "revision": "30b76e2ee02745bf1698",
+    "url": "/MW_assignment/static/js/runtime-main.6ca14e54.js"
+  }
+]);
